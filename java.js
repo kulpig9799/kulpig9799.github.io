5bdda3567d8333bee09fd9347a862ee215ca0344
kulpig9799 <nguyenhoangcong152@gmail.com>
2019-07-03 08:26:07 +0700

    Ảnh

---
+++ java.js
@@ -0,0 +1,42 @@
+var images = ["img/DSC01524.jpg","img/DSC01525.jpg","img/DSC01526.jpg","img/DSC01539.jpg","img/DSC01541.jpg","img/DSC01542.jpg","img/DSC01548.jpg","img/DSC01549.jpg"]
+
+for ( i = 0; i < images.length; i++ ){
+  $(".slideshow-container").append(`
+  <div class="mySlides fade">
+  <img src="${images[i]}" style="width:100%;max-width:900px">
+</div>
+`)
+}
+
+//khai báo biến slideIndex đại diện cho slide hiện tại
+var slideIndex;
+// KHai bào hàm hiển thị slide
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    // var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    // for (i = 0; i < dots.length; i++) {
+    //     dots[i].className = dots[i].className.replace(" active", "");
+    // }
+
+    slides[slideIndex].style.display = "block";  
+    // dots[slideIndex].className += " active";
+    //chuyển đến slide tiếp theo
+    slideIndex++;
+    //nếu đang ở slide cuối cùng thì chuyển về slide đầu
+    if (slideIndex > slides.length - 1) {
+      slideIndex = 0
+    }    
+    //tự động chuyển đổi slide sau 5s
+    setTimeout(showSlides, 3000);
+}
+//mặc định hiển thị slide đầu tiên 
+showSlides(slideIndex = 0);
+
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
